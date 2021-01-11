cc1e1c239cfdfac1567e9d0b4c869ee1988561f1
Kgpalli <kalpana.gollapalli02@gmail.com>
2021-01-10 21:42:14 -0800

    Pininterest Project setup

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="app_header">
+            <div className="header_wrapper"></div>
+            <div className="header_logo">
+                <p>Logo</p>
+            </div>
+            <div className="header_button homepage">
+                <p>Home Page</p>
+            </div>
+            <div class="header_button following">
+                <p> following</p> </div>
+ <div className="header_search">
+     <p>SearchIcon</p></div>
+
+ </div>
+    
+    )
+}
+
+export default Header
